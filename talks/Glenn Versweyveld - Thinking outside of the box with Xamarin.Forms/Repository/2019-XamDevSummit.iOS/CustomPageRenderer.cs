dde70ade5f3c8494d5e6406e0d69fc4362bb6e15
Depechie <glenn.versweyveld@gmail.com>
2019-12-04 15:01:51 +0100

    iOS13 model pagesheet style

---
+++ talks/Glenn Versweyveld - Thinking outside of the box with Xamarin.Forms/Repository/2019-XamDevSummit.iOS/CustomPageRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using _2019_XamDevSummit.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Page), typeof(CustomPageRenderer))]
+namespace _2019_XamDevSummit.iOS
+{
+    public class CustomPageRenderer : PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+        }
+
+        public override void DidMoveToParentViewController(UIViewController parent)
+        {
+            base.DidMoveToParentViewController(parent);
+            //Avoid new iOS13 modal PageSheet style
+            parent.ModalPresentationStyle = UIModalPresentationStyle.FullScreen;
+        }
+    }
+}
